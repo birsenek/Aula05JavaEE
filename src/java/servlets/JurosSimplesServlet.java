58638b1446b9ced0cd13084988a375742fd4738d
birsenek <birsenek@gmail.com>
2021-03-08 20:12:15 -0300

    Initial commit

---
+++ src/java/servlets/JurosSimplesServlet.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.text.DecimalFormat;
+
+/**
+ *
+ * @author BirseneK
+ */
+@WebServlet(name = "JurosSimplesServlet", urlPatterns = {"/juros-simples.html"})
+public class JurosSimplesServlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Juros Simples Servlet</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            double vp = Double.parseDouble(request.getParameter("valorPresente"));
+            double j = Double.parseDouble(request.getParameter("juros"));
+            double n = Double.parseDouble(request.getParameter("tempo"));
+            DecimalFormat numberFormat = new DecimalFormat("#.00"); 
+            out.println("<h1>Servlet Juros Simples</h1>");
+            out.println("<h2>Valor presente: R$ " + vp + "</h2>");
+            out.println("<h2>Juros(em %): " + j + "</h2>");
+            j = j/100;
+            out.println("<h2>Tempo (em meses): " + n + "</h2>");
+            double vf = vp * (1+(j*n));
+            out.println("<h2>Valor Futuro: R$ " + numberFormat.format(vf) + "</h2>");
+            out.println("<a href='index.html'><button>Retornar</button></a>");
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
